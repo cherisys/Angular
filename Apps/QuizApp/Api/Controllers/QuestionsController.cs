7021b3ebc7077fd20b8d8f856b513329d5cee18c
cherisys <arshad@cherisys.com>
2018-10-21 04:48:43 +0530

    Api Updated.

---
+++ Apps/QuizApp/Api/Controllers/QuestionsController.cs
@@ -0,0 +1,26 @@
+﻿using Api.Global;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private IRepository<Question> Repo;
+
+        public QuestionsController(IRepository<Question> _repo)
+        {
+            Repo = _repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var QuestionList = await Repo.GetAll();
+            return Ok(QuestionList);
+        }
+    }
+}
--- Apps/QuizApp/Api/Startup.cs
+++ Apps/QuizApp/Api/Startup.cs
@@ -1,46 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Api.Global;
+using Api.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 
 namespace Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().AddJsonOptions(a => a.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+
+            services.AddDbContext<ApiDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //using Dependency Injection
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
+            app.ApplyUserKeyValidation();
             app.UseMvc();
         }
     }
