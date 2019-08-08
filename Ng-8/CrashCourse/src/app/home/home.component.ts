c2e4d9f8f233ad7ed5af998f69c76c6a1631da75
cherisys <arshad@cherisys.com>
2019-08-08 13:35:58 +0530

    Crash Course Build

---
+++ Ng-8/CrashCourse/src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  h1Style:boolean;
+  users:object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+  }
+
+  firstClick(){
+    this.h1Style = true;
+    this.data.firstClick().subscribe(data=> {
+      this.users = data;
+      console.log(this.users);
+
+//  Returned Data Object
+//       {data: Array(3) [ {…}, {…}, {…} ],
+// ​       page: 1,
+// ​       per_page: 3,
+// ​       total: 12,
+// ​       total_pages: 4}
+      
+    });
+  }
+}
